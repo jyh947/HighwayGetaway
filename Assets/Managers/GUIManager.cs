e3cd3f9d5998305cbe4701308a2800101cedfb83
jyh947 <jyh947@gmail.com>
2014-12-30 17:21:22 -0500

    Basic Title Stuff

--- Assets/Managers/GUIManager.cs
+++ Assets/Managers/GUIManager.cs
@@ -2,5 +2,47 @@
 
 public class GUIManager : MonoBehaviour {
 	
-	public GUIText titleText, tryAgainText;
+	public GUIText velocityText, scoreText, policeDistanceText, tryAgainText;
+	public GUITexture Title;
+	
+	private static GUIManager instance;
+	
+	void Start () {
+		instance = this;
+		GameEventManager.Game += Game;
+		GameEventManager.GameOver += GameOver;
+		Title.enabled = true;
+		velocityText.enabled = false;
+		scoreText.enabled = false;
+		policeDistanceText.enabled = false;
+		tryAgainText.enabled = false;
+	}
+	
+	void Update () {
+		if(Input.GetButtonDown("Jump")){
+			GameEventManager.TriggerGame();
+		}
+		if(Input.GetButtonDown("Fire1")){
+			GameEventManager.TriggerGameOver();
+		}
+	}
+
+	private void Game() {
+		Title.enabled = false;
+		velocityText.enabled = true;
+		scoreText.enabled = true;
+		policeDistanceText.enabled = true;
+		tryAgainText.enabled = false;
+		enabled = false;
+
+	}
+	
+	private void GameOver () {
+		Title.enabled = true;
+		velocityText.enabled = false;
+		scoreText.enabled = false;
+		policeDistanceText.enabled = false;
+		tryAgainText.enabled = true;
+		enabled = true;
+	}
 }
---
+++ Assets/Managers/GameEventManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class GameEventManager {
+	
+	public delegate void GameEvent();
+	
+	public static event GameEvent TitleScreen, Game, GameOver;
+
+
+	public static void TriggerGame(){
+		if(Game != null){
+			Game();
+		}
+	}
+	
+	public static void TriggerGameOver(){
+		if(GameOver != null){
+			GameOver();
+		}
+	}
+}

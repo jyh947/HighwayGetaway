d537fc9706c1f8c0faef3950cf28a787bf5b16aa
Yogesh Seetharaman <yogeshs@umich.edu>
2014-12-29 22:21:26 -0500

    add base

---
+++ Assets/Car/Car.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Car : MonoBehaviour {
+
+	public static float distanceTravelled;
+
+	// Use this for initialization
+	void Start () {
+		distanceTravelled = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(0f, 0f, 5f * Time.deltaTime);
+		distanceTravelled = transform.localPosition.z;
+	}
+}
---
+++ Assets/Game Scripts/CameraManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraManager : MonoBehaviour {
+
+	public float cameraX;
+	public float cameraY;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(cameraX, cameraY, Car.distanceTravelled);
+	}
+}

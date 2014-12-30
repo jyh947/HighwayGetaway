d4692d32f1a7cb380b686cee95c1f0fddd13ac78
Matthew Conto <conto@nyu.edu>
2014-12-30 00:41:40 -0500

    added lane, lane manager, and NPCar

---
+++ Assets/Car/LaneManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LaneManager : MonoBehaviour {
+
+	public static float MAXIMUM_CAR_DISTANCE = 100f;
+	public static float MINIMUM_CAR_SPAWN_DISTANCE = 10f;
+	public float carSpawnFrequency = 0;
+	
+	public List<Lane> lanes = new List<Lane>();
+	public List<NonPlayerCar> cars = new List<NonPlayerCar>();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

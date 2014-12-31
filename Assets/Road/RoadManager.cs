f6bfd8934e5960e51afa823aa7581c94d57e7898
Yogesh Seetharaman <yogeshs@umich.edu>
2014-12-30 23:48:03 -0500

    auto matic road generation only minor changes needed =)

--- Assets/Road/RoadManager.cs
+++ Assets/Road/RoadManager.cs
@@ -3,46 +3,122 @@ using System.Collections.Generic;
 
 public class RoadManager : MonoBehaviour {
 	
-	public Transform roadPrefab;
+	public Transform leftBorderPrefab;
+	public Transform rightBorderPrefab;
 	public Transform dividerPrefab;
+	public Transform roadTilePrefab;
 
-	public int numRows;
-	public int rowWidth;
-	public float recycleOffset;
+	public float pubTileWidth = 1.75f;
+
+	public int minLanes = 2;
+	public int maxLanes = 4;
+	public int numLanes = 2;
+	public int roadLength = 60;
+	public int recycleOffset = 5;
+
+	public int maxRoadTileObjects = 60;
 	
 	public Vector3 startPosition;
 	private Vector3 nextPosition;
 	
-	private Queue<Transform> tileQueue;
-	
+	private Queue<Transform> leftBorderQueue;
+	private Queue<Transform> rightBorderQueue;
+	private Queue<Transform> dividerQueue;
+	private Queue<Transform> roadTileQueue;
+
+	private float tileHeight;
+	private float borderWidth;
+	private float dividerWidth;
+	private float roadTileWidth;
+
 	// Use this for initialization
 	void Start () {
-		nextPosition = startPosition;
-		
-		tileQueue = new Queue<Transform> (numRows * rowWidth);
-		for (int i = 0; i < numRows * rowWidth; ++i) {
-			tileQueue.Enqueue ((Transform)Instantiate (roadPrefab));
+		// Construct queues
+		leftBorderQueue = new Queue<Transform> (maxRoadTileObjects);
+		rightBorderQueue = new Queue<Transform> (maxRoadTileObjects);
+		dividerQueue = new Queue<Transform> (maxRoadTileObjects);
+		roadTileQueue = new Queue<Transform> (maxRoadTileObjects);
+
+		// Generate road assets
+		for (int i = 0; i < maxRoadTileObjects; ++i) {
+			Debug.LogWarning("made obj");
+			leftBorderQueue.Enqueue((Transform)Instantiate(leftBorderPrefab));
+			rightBorderQueue.Enqueue((Transform)Instantiate(rightBorderPrefab));
+			dividerQueue.Enqueue((Transform)Instantiate(dividerPrefab));
+			roadTileQueue.Enqueue((Transform)Instantiate(roadTilePrefab));
 		}
-		for (int i = 0; i < numRows; ++i) {
-			recycle();
+
+		// Get prefab dimensions
+		Renderer r;
+		
+		r = leftBorderQueue.Peek().GetComponent<Renderer>();
+		borderWidth = r.bounds.size.x;
+
+		r = dividerQueue.Peek().GetComponent<Renderer>();
+		dividerWidth = r.bounds.size.x;
+
+		r = roadTileQueue.Peek().GetComponent<Renderer> ();
+		roadTileWidth = r.bounds.size.x;
+
+		tileHeight = r.bounds.size.z;
+		/*
+		Debug.Log ("borderWidth: " + borderWidth);
+		Debug.Log ("dividerWidth: " + dividerWidth);
+		Debug.Log ("roadTileWidth: " + roadTileWidth);
+		Debug.Log ("tileHeight: " + tileHeight);
+		*/
+
+		// set tiles to locations
+		for (int i = 0; i < maxRoadTileObjects; ++i) {
+			constructRoad();
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (tileQueue.Peek().localPosition.z + recycleOffset < Car.getDistanceTravelled()) {
-			recycle();
+		/*
+		Debug.Log (roadTileQueue.Count);
+		Debug.Log (leftBorderQueue.Count);
+		Debug.Log (rightBorderQueue.Count);
+		Debug.Log (dividerQueue.Count);
+		*/
+		if (leftBorderQueue.Peek ().localPosition.z + recycleOffset * tileHeight < Car.getDistanceTravelled ()) {
+			constructRoad ();
 		}
 	}
-	
-	private void recycle() {
-		for (int i = 0; i < rowWidth; ++i) {
-			Transform o = tileQueue.Dequeue ();
-			o.localPosition = nextPosition;
-			nextPosition.x += o.localScale.x;
-			tileQueue.Enqueue (o);
+
+	private void constructRoad() {
+		Transform border = leftBorderQueue.Dequeue ();
+		border.localPosition = nextPosition;
+		leftBorderQueue.Enqueue (border);
+		nextPosition.x += pubTileWidth;
+
+		Transform roadTile, divider;
+		Debug.Log ("numlanes: " + numLanes);
+		for (int i = 0; i < numLanes - 1; ++i) {
+			Debug.Log("made lane");
+			roadTile = roadTileQueue.Dequeue();
+			roadTile.localPosition = nextPosition;
+			roadTileQueue.Enqueue(roadTile);
+			nextPosition.x += pubTileWidth;
+
+			divider = dividerQueue.Dequeue();
+			divider.localPosition = nextPosition;
+			dividerQueue.Enqueue(divider);
+			nextPosition.x += pubTileWidth;
 		}
-		nextPosition.z += tileQueue.Peek ().localScale.z;
+		Debug.Log ("over");
+
+		roadTile = roadTileQueue.Dequeue();
+		roadTile.localPosition = nextPosition;
+		roadTileQueue.Enqueue(roadTile);
+		nextPosition.x += pubTileWidth;
+
+		border = rightBorderQueue.Dequeue ();
+		border.localPosition = nextPosition;
+		rightBorderQueue.Enqueue (border);
+
 		nextPosition.x = startPosition.x;
+		nextPosition.z += tileHeight;
 	}
 }

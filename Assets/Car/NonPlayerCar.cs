2ba1020016f2a80adb2fbbafd0865fec73242f49
Yogesh Seetharaman <yogeshs@umich.edu>
2014-12-31 13:56:12 -0500

    minor changes

--- Assets/Car/NonPlayerCar.cs
+++ Assets/Car/NonPlayerCar.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class NonPlayerCar : MonoBehaviour {
 	
 	public float speed;
-	private float relativeSpeed = speed - ;
+	//private float relativeSpeed = speed - ;
 
 	public void stopCar(){
 		// 

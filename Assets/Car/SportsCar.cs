5bcf077f072effded4133d009b2ac4c57098df6f
jyh947 <jyh947@gmail.com>
2015-01-02 00:23:21 -0500

    Summons one Sports car

---
+++ Assets/Car/SportsCar.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SportsCar : MonoBehaviour {
+
+	public float startX = 0f;
+	public float startY = 0f;
+	public float laneSwitchOffset = 3.375f;
+	public static float speed = 7f;
+
+	// Use this for initialization
+	void Start () {
+		// Generate random numbers about where to start
+		int startingLane = (int)Random.Range (0, RoadManager.numLanes - 1);
+		//int startingLane = 1;
+		transform.position = new Vector3 (startX + startingLane * laneSwitchOffset, startY,
+		                                  Car.getDistanceTravelled() + 45f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(0f, 0f, speed * Time.deltaTime);
+		if (transform.position.z < Car.getDistanceTravelled () - 20f) {
+			//destroy car here
+		}
+	}
+}
--- Assets/Game Scripts/CameraManager.cs
+++ Assets/Game Scripts/CameraManager.cs
@@ -13,6 +13,6 @@ public class CameraManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = new Vector3(cameraX, cameraY, Car.getDistanceTravelled());
+		transform.position = new Vector3(cameraX, cameraY, Car.getDistanceTravelled() - 5f);
 	}
 }
